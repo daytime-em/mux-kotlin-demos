7d5361a7d2f287719266db24b5c23abf5fb36aaa
Emily Dixon <edixon@mux.com>
2021-10-21 19:34:54 -0700

    Add JwPlayer Demo

---
+++ MuxJwPlayerDemo/src/main/java/com/mux/muxjwplayerdemo/JwPlayerMainActivity.kt
@@ -0,0 +1,11 @@
+package com.mux.muxjwplayerdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JwPlayerMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jw_player_main)
+    }
+}
