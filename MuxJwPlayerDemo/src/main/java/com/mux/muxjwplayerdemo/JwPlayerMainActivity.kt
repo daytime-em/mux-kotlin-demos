dd46bc1c0c01d1a5ecb3e76528475f735a1bd752
Emily Dixon <edixon@mux.com>
2021-10-21 21:17:59 -0700

    Add JWPlayer Libs

--- MuxJwPlayerDemo/src/main/java/com/mux/muxjwplayerdemo/JwPlayerMainActivity.kt
+++ MuxJwPlayerDemo/src/main/java/com/mux/muxjwplayerdemo/JwPlayerMainActivity.kt
@@ -2,10 +2,67 @@ package com.mux.muxjwplayerdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.longtailvideo.jwplayer.JWPlayerView
+import com.longtailvideo.jwplayer.configuration.PlayerConfig
+import com.longtailvideo.jwplayer.license.LicenseUtil
+import com.longtailvideo.jwplayer.media.playlists.PlaylistItem
 
 class JwPlayerMainActivity : AppCompatActivity() {
+
+    lateinit var playerView: JWPlayerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // TODO: Replace with [YOUR KEY HERE]
+        LicenseUtil.setLicenseKey(this, "5QwWG43Fz8JBm7dfyXaNI5wfNKHhzj88/ypd+ze445ufhExCdApPGX5KbGw=")
+
         setContentView(R.layout.activity_jw_player_main)
+
+        playerView = findViewById(R.id.jwplayerview)
+        playerView.setup(createPlayerConfig(createPlaylist()))
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        playerView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        playerView.onResume()
+    }
+
+    override fun onPause() {
+        playerView.onPause()
+
+        super.onPause()
+    }
+
+    override fun onStop() {
+        playerView.onStop()
+
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        playerView.onDestroy()
+
+        super.onDestroy()
+    }
+
+    private fun createPlayerConfig(playlist: List<PlaylistItem>): PlayerConfig =
+        PlayerConfig.Builder()
+            .playlist(playlist)
+            .build()
+
+    private fun createPlaylist(): List<PlaylistItem> =
+        listOf(
+            PlaylistItem.Builder()
+                .title("Mux Sample Video")
+                .file("https://cdn.jwplayer.com/manifests/Z6NjJRb2.m3u8")
+                .build(),
+        )
 }
